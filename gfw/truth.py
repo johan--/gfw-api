4d1a15bdd97ed293d2f906428a89edbf4d8e199d
Dan Hammer <dan.s.hammer@gmail.com>
2014-03-17 17:35:29 -0700

    WIP: added the dictionary for landsat stack, broke API call

--- gfw/truth.py
+++ gfw/truth.py
@@ -89,8 +89,24 @@ def landsatMedian(alert_date, coords, offset_days=90):
     begin_date = d - datetime.timedelta(days=offset_days)
     poly = ee.Feature.Polygon(coords)
     coll = ee.ImageCollection('LANDSAT/LC8_L1T_TOA').filterDate(begin_date, alert_date)
-    clipped = coll.map(lambda x: x.clip(poly))
-    return clipped.median()
+    return coll.clip(poly).median()
+
+def _img_url(image_id, coords):
+    """Accepts an image_id and the coordinates for a bounding box;
+    returns the URL
+
+    """
+    loc = 'LANDSAT/%s' % image_id
+    input = cloudMask(ee.Image(loc))
+    rgb = input.select("B6","B5","B4")
+    pan = input.select("B8")
+    sharp = hsvpan(rgb, pan)
+
+    vis_params = {'min':0.01, 'max':0.5, 'gamma':1.7}
+    visual_image = sharp.visualize(**vis_params)
+    params = {'scale':30, 'crs':'EPSG:4326', 'region':str(coords)}
+    url = visual_image.getThumbUrl(params)
+    return url
 
 def _boom_hammer(lat, lon, h, w, date, res, asset, fmt):
     """Return URL to Earth Engine results for supplied params dictionary.
@@ -104,21 +120,23 @@ def _boom_hammer(lat, lon, h, w, date, res, asset, fmt):
         res - desired resolution (thumb | true)
         asset - Earth Engine asset
         fmt - desired output format (img | raw)"""
-    
     coords = createBox(lon, lat, w, h)
-    image_id = landsatID(date, coords)
-    loc = 'LANDSAT/%s' % image_id
 
-    input = cloudMask(ee.Image(loc)) # TODO: Should we use a cloud masking algorithm?
-    rgb = input.select("B6","B5","B4")
-    pan = input.select("B8")
-    sharp = hsvpan(rgb, pan)
+    def _get(d):
+        str_date = datetime.datetime.strftime(d, '%Y-%m-%d')
+        return _img_url(landsatID(str_date, coords), coords)
 
-    vis_params = {'min':0.01, 'max':0.5, 'gamma':1.7}
-    visual_image = sharp.visualize(**vis_params)
-    params = {'scale':30, 'crs':'EPSG:4326', 'region':str(coords)}
-    url = visual_image.getThumbUrl(params)
-    return url
+    init_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+
+    t_minus_one = init_date - datetime.timedelta(days=30) 
+    t_minus_two = init_date - datetime.timedelta(days=60) 
+    t_minus_three = init_date - datetime.timedelta(days=90) 
+
+    return {'alert_date': _get(init_date), 
+            't_minus_one': _get(t_minus_one), 
+            't_minus_two': _get(t_minus_two), 
+            't_minus_three': _get(t_minus_three)}
+
 
 def _params_prep(params):
     """Return prepared params ready to go as dict."""
@@ -147,4 +165,4 @@ def find(params):
     ee.data.setDeadline(60000)
     url = _boom_hammer(**boom)
     result = _fetch_url(url)
-    return result
+    return url
