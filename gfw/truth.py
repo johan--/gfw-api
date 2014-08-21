a72066baed82bf2440bf5a6aa330c06ee29a1429
Dan <dan.s.hammer@gmail.com>
2014-08-21 12:27:44 -0700

    added geometry, hotfix, for the love

--- gfw/truth.py
+++ gfw/truth.py
@@ -117,7 +117,13 @@ def pixelCloudScore(img):
 def cloudScore(img):
     pix = pixelCloudScore(img)
     binary = pix.gt(0.5)
-    num = binary.reduceRegion(ee.Reducer.mean(), scale=30, crs='EPSG:4326')
+
+    num = binary.reduceRegion(
+        reducer=ee.Reducer.mean(), 
+        geometry=pix.geometry(), 
+        scale=30, 
+        crs='EPSG:4326'
+    )
     return num.getInfo()['constant']
 
 
