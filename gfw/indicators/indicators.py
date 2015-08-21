466d30654a01cc3880bbfbd0b9029a04850aa84a
Simao Belchior <simaobelchior@gmail.com>
2015-08-21 16:54:53 +0100

    removes unnecessary code from copy paste

--- gfw/indicators/indicators.py
+++ gfw/indicators/indicators.py
@@ -20,92 +20,22 @@
 import json
 
 from gfw import cdb
-from gfw.forestchange import umd
 
 
-class CountrySql(object):
+class IndicatorSql(object):
 
     INDEX = """
-        SELECT countries.iso, countries.name, countries.enabled, countries.lat,
-        countries.lng, countries.extent, countries.gva, countries.gva_percent,
-        countries.employment, countries.indepth, countries.national_policy_link,
-        countries.national_policy_title, countries.convention_cbd,
-        countries.convention_unfccc, countries.convention_kyoto,
-        countries.convention_unccd, countries.convention_itta,
-        countries.convention_cites, countries.convention_ramsar,
-        countries.convention_world_heritage, countries.convention_nlbi,
-        countries.convention_ilo, countries.ministry_link, countries.external_links,
-        countries.dataset_link, countries.emissions, countries.carbon_stocks,
-        countries.country_alt, alerts.count AS alerts_count
-        FROM gfw2_countries AS countries
-        LEFT OUTER JOIN (
-          SELECT COUNT(*) AS count, iso
-          FROM forma_api
-          WHERE date >= now() - INTERVAL '{interval}'
-          GROUP BY iso)
-        AS alerts ON alerts.iso = countries.iso
-        ORDER BY countries.name {order}
+        SELECT *
+        FROM indicators
+        LIMIT 20
     """
 
     SHOW = """
-        SELECT countries.iso, countries.name, countries.enabled, countries.lat,
-        countries.lng, countries.extent, countries.gva, countries.gva_percent,
-        countries.employment, countries.indepth, countries.national_policy_link,
-        countries.national_policy_title, countries.convention_cbd,
-        countries.convention_unfccc, countries.convention_kyoto,
-        countries.convention_unccd, countries.convention_itta,
-        countries.convention_cites, countries.convention_ramsar,
-        countries.convention_world_heritage, countries.convention_nlbi,
-        countries.convention_ilo, countries.ministry_link, countries.external_links,
-        countries.dataset_link, countries.emissions, countries.carbon_stocks,
-        countries.country_alt
-        FROM gfw2_countries AS countries
-        WHERE countries.iso = UPPER('{iso}')
+        SELECT *
+        FROM indicators
+        WHERE id = {id}
         LIMIT 1
     """
-
-    TOPO_JSON = """
-        SELECT the_geom
-        FROM forest_cov_glob_v3
-        WHERE country_code = UPPER('{iso}')
-        UNION
-        SELECT the_geom
-        FROM ne_50m_admin_0_countries
-        WHERE adm0_a3 = UPPER('{iso}')"""
-
-    SUBNAT_BOUNDS = """
-        SELECT cartodb_id, iso, id_1, name_1, bounds
-        FROM gadm_1_all
-        WHERE iso = UPPER('{iso}')
-        ORDER BY id_1 asc"""
-
-    TENURE = """
-        SELECT tenure_government, tenure_owned, tenure_owned_individuals,
-            tenure_reserved, GREATEST(tenure_government, tenure_owned,
-            tenure_owned_individuals, tenure_owned_individuals,
-            tenure_reserved) AS max
-        FROM gfw2_countries
-        WHERE iso = UPPER('{iso}')"""
-
-    FORESTS = """
-        SELECT unnest(array['forest_regenerated', 'forest_primary',
-               'forest_planted']) AS type, unnest(array[COALESCE(
-               forest_regenerated, 0), COALESCE(forest_primary, 0), COALESCE(
-               forest_planted, 0)]) AS percent
-        FROM gfw2_countries
-        WHERE iso = UPPER('{iso}')"""
-
-    FORMA = """
-        SELECT date_trunc('month', date) AS date, COUNT(*) AS alerts
-        FROM forma_api
-        WHERE iso = UPPER('{iso}')
-        GROUP BY date_trunc('month', date)
-        ORDER BY date_trunc('month', date) ASC"""
-
-    BOUNDS = """
-        SELECT bounds
-        FROM country_mask
-        WHERE code = UPPER('{iso}')"""
 
 
 def _handler(response):
@@ -133,55 +63,6 @@ def _show(args):
     rows = _handler(cdb.execute(query))
     return rows[0]
 
-def _getTopoJson(args):
-    query = CountrySql.TOPO_JSON.format(**args)
-
-    rows = _handler(
-        cdb.execute(query, params=dict(format='topojson')))
-    return dict(topojson=rows)
-
-def _processSubnatRow(x):
-    x['bounds'] = json.loads(x['bounds'])
-    return x
-
-
-def _getSubnatBounds(args):
-    query = CountrySql.SUBNAT_BOUNDS.format(**args)
-
-    rows = _handler(cdb.execute(query))
-    results = map(_processSubnatRow, rows)
-    return dict(subnat_bounds=results)
-
-
-def _getForma(args):
-    query = CountrySql.FORMA.format(**args)
-
-    return dict(forma=_handler(cdb.execute(query)))
-
-
-def _getForests(args):
-    query = CountrySql.FORESTS.format(**args)
-
-    return dict(forests=_handler(cdb.execute(query)))
-
-
-def _getTenure(args):
-    query = CountrySql.TENURE.format(**args)
-
-    return dict(tenure=_handler(cdb.execute(query)))
-
-
-def _getBounds(args):
-    query = CountrySql.BOUNDS.format(**args)
-
-    return dict(bounds=json.loads(_handler(cdb.execute(query))[0]['bounds']))
-
-
-def _getUmd(args):
-    action, data = umd.execute(args)
-
-    return dict(umd=data['years'])
-
 
 def execute(args):
     result = dict(params=args)
@@ -191,12 +72,5 @@ def execute(args):
 
     else:
         result.update(_show(args))
-        result.update(_getTopoJson(args))
-        result.update(_getSubnatBounds(args))
-        result.update(_getForma(args))
-        result.update(_getForests(args))
-        result.update(_getTenure(args))
-        result.update(_getBounds(args))
-        result.update(_getUmd(args))
 
-    r
+    return 'respond', result
